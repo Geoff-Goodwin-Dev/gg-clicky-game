28d0f3d2c109199742700bc7be9d622a152ff76e
Geoff-Goodwin-Dev <ggoodwin.dev@gmail.com>
2018-06-23 13:51:27 -0400

    updates shuffling and win loss conditions

--- src/App.js
+++ src/App.js
@@ -6,15 +6,55 @@ import './app.css';
 class App extends Component {
   state = {
     characters,
+    score: 0,
+    wins: 0,
+    losses: 0
   };
 
   clickedTile = id => {
-    let charactersPreClick = this.state.characters;
-    let clickedFriend = charactersPreClick.find((o, i) => {
-      return (o.id === id) ? charactersPreClick[i].clicked = true : console.log("couldn't find it");
+    let charactersArray = this.state.characters;
+    const newScore = this.state.score + 1;
+    charactersArray.find((o, i) => {
+      if (o.id === id && !o.clicked) {
+        charactersArray[i].clicked = true;
+        charactersArray = this.shuffleArray(charactersArray);
+        this.setState({characters, score: newScore});
+        if (newScore === 9) {
+          this.handleRoundReset();
+          return this.setState({wins: this.state.wins +1});
+        }
+        return true;
+      }
+      else if (o.id === id && o.clicked) {
+        return this.handleAlreadyClicked(charactersArray[i].name);
+      }
+      else {
+        return false;
+      }
     });
-    console.log(clickedFriend);
-    this.setState({ characters });
+  };
+
+  shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+
+  };
+
+  handleAlreadyClicked = (name) => {
+    console.log(`${name} has already been clicked!`);
+    this.handleRoundReset();
+    return this.setState({losses: this.state.losses + 1})
+  };
+
+  handleRoundReset = () => {
+    const charactersArray = this.state.characters;
+    for (let character of charactersArray) {
+      character.clicked = false
+    }
+    return this.setState({characters, score: 0});
   };
 
   render() {
@@ -22,6 +62,9 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <h2>Clicky Game</h2>
+          <h3>Score: {this.state.score}</h3>
+          <h3>Wins: {this.state.wins}</h3>
+          <h3>Losses: {this.state.losses}</h3>
           <hr />
           <div className="tileHolder">
             {this.state.characters.map(character => (
@@ -34,7 +77,6 @@ class App extends Component {
                 clicked={character.clicked}
               />
             ))}
-            {console.log(this.state.characters)}
           </div>
         </div>
       </div>
